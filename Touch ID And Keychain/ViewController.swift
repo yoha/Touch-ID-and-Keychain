aa6f22f2b2a1367ff15437fea73ac92750a9d456
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-10-19 09:56:17 +0700

    Setup notificationCenter to be alerted when keyboard show / hide / change frame & what to do about it via local method.

--- Touch ID And Keychain/ViewController.swift
+++ Touch ID And Keychain/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlet Properties
+    
+    @IBOutlet weak var secretTextView: UITextView!
+    
+    // MARK: - IBAction Properties
+    
+    @IBAction func authenticateUser(sender: UIButton) {
+    }
+    
+    // MARK: - Methods Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard", name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard", name: UIKeyboardWillChangeFrameNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Local Methods
+    
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardViewEndFrame = self.view.convertRect(keyboardScreenEndFrame, fromView: self.view.window!)
+        
+        if notification.name == UIKeyboardWillHideNotification {
+            self.secretTextView.contentInset = UIEdgeInsetsZero
+        }
+        else {
+            self.secretTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        self.secretTextView.scrollIndicatorInsets = self.secretTextView.contentInset
+        
+        let selectedRange = self.secretTextView.selectedRange
+        self.secretTextView.scrollRangeToVisible(selectedRange)
+    }
 }
 
